3358fa3775fbcacb4f6bb7a3cadc46bfc660de1e
Herik <herikfernandes100@gmail.com>
2026-09-11 13:42:32 -0300

    servidorImage

--- src/config/portfolio.jsx
+++ src/config/portfolio.jsx
@@ -19,6 +19,7 @@ import {
   SiMongodb,
 } from "react-icons/si";
 import avatarImage from "../assets/Foto.JPEG";
+import servidorImage from "../assets/servidor.png";
 import calculadoraImage from "../assets/calculadora.png";
 import ludoImage from "../assets/ludo.png";
 import portfolioImage from "../assets/portfolio.png";
